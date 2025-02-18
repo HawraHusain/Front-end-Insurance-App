d12f41df89ecbccb85d53caa80b10d8500503e63
HawraHusain <hawraanowrooz158@gmail.com>
2025-02-18 20:40:43 +0300

    Added the list and show

--- src/services/companyService.js
+++ src/services/companyService.js
@@ -2,16 +2,47 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/company`;
 
 
 const index = async () => {
-    try {
-        const res = await fetch(BASE_URL, {
-          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-        });
-        return res.json();
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
 
-    export default {
+    const data = await res.json();
+
+    console.log(data);
+
+    if (data.err) {
+      throw new Error(data.err);
+    }
+    console.log(data);
+    return data
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+const show = async (companyId) => {
+    try{
+        const res = await fetch(`${BASE_URL}/${companyId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+          });
+      
+          const data = await res.json();
+      
+          console.log(data);
+      
+          if (data.err) {
+            throw new Error(data.err);
+          }
+          console.log(data);
+          return data
+    }
+    catch(err){
+        console.log(err);
+        throw new Error(err);
+    }
+}
+    export {
         index,
+        show,
       };
